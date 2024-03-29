363157905ea9c7a26660ad9e4af8bffec6b9e3fc
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-28 23:43:01 -0400

    Added links to new pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,31 +16,42 @@ function Navbar() {
   //  depending on whether the user is logged in or not
   return (
     <nav>
-    {/* <h1 style={{textAlign:"center" }}> <img style={{height:"150px" }}src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711642893/quoteFrame_gcgjen.png" alt="quote frame"/> 
+      {/* <h1 style={{textAlign:"center" }}> <img style={{height:"150px" }}src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711642893/quoteFrame_gcgjen.png" alt="quote frame"/> 
     isDemocracy ||
     <img style={{height:"100px" }} src='https://res.cloudinary.com/dw1igjvgi/image/upload/v1711639080/dadaFrame_nwectm.png' alt="Dada in bold black letters" />
     </h1> */}
       <Link to="/">
         <button>Home</button>
       </Link>
+      -
       <Link to="/billlookup">
         <button>Bill Search</button>
       </Link>
-
+      -
+      <Link to="/dadaists">
+        <button>Our community </button>
+      </Link>
+      -
+      <Link to="/forum">
+        <button>BuzzBoard</button>
+      </Link>
+      -
       {getToken() ? (
         <>
-          <button onClick={logOutUser}>Logout</button>
+          <Link to="/user">
+            <button>UserDashboard</button>
+          </Link>
+          -<button onClick={logOutUser}>Logout</button>
           {/* <span>{user && user.name}</span> */}
         </>
       ) : (
         <>
           <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
+            <button>Sign Up</button>
           </Link>
+          -
           <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
+            <button>Login</button>
           </Link>
         </>
       )}
