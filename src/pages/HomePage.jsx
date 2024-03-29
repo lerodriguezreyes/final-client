eecb8065c48031dc32dd66dd8f8a6e7a57e92986
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-28 21:35:57 -0400

    draft of homepage - needs styling.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import YoutubeEmbed from "../components/YoutubeEmbed";
+
+
 function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h2>So this is how "they" make bills...</h2>
+      <YoutubeEmbed embedId="tyeJ55o3El0"/>
+      <h2>| 290,007 bills in 50 years | Where do we fit in this process? |</h2>
+      <h2>Let's talk about this!</h2>
     </div>
   );
 }
