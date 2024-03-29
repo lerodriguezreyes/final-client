dcba0f7decab545396b36da934bb8382b8686565
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-28 21:36:21 -0400

    included bill search functionality

---
+++ src/pages/BillLookup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProPub from '../components/ProPub'
+
+function BillLookup() {
+  return (
+    <div>
+    <h2>BillLookup</h2>
+    <ProPub />
+    </div>
+  )
+}
+
+export default BillLookup
