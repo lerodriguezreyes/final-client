aa632a247b28d858183fcfe4ebb8955e93695bd0
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-28 21:35:06 -0400

    fixed unread api key bug

--- src/services/ProPublicaAPI.js
+++ src/services/ProPublicaAPI.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const propubget = (query, offset) => {
   
     return axios.get(`https://api.propublica.org/congress/v1/bills/search.json?query=${query}&offset=${offset}`, {
-      headers: { 'X-API-Key': import.meta.env.PROPUB_API_KEY },
+      headers: { 'X-API-Key': import.meta.env.VITE_PROPUB_API_KEY },
     })
   };
