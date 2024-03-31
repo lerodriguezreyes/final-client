534c6f4ab5ced3e7e393c4ad3d7472986d2e2c01
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-31 14:08:42 -0400

    finished footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,47 @@
+import "../styles/footer.css";
+
 function Footer() {
   return (
-    <div id="Footer">
-      <p id="p1">
-        Made with{" "}
-        {
-          <img
-            width="20"
-            height="20"
-            src="https://github.com/lerodriguezreyes/react-kanban/blob/main/images/love.png?raw=true"
-            alt="filled-like"
-          />
-        }
-        by Luis Emmanuel
-      </p>
-      <p id="p2">
-        <a
-          href="https://github.com/lerodriguezreyes/final-client"
-          target= "_blank"
-        >
-          {" "}
-          SPA Repository{" "}
-        </a>
-        <a
-          href="https://github.com/lerodriguezreyes/final-server"
-          target= "_blank"
-        >
-          {" "}
-          Mock server repository{" "}
-        </a>
-        <a
-          href="mailto:lerodriguezreyes@outlook.com?subject=About%20is%20Democracy%20%20"
-        >
-          {" "}
-          Reach Out!{" "}
-        </a>
-      </p>
+    <div className="footerContainer">
+      <img
+        id="footerFrame"
+        src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711644483/quoteFrame180_fohqjp.png"
+        alt="quote frame"
+      />
+      <div className="footerText">
+        <p id="p1">
+          Made with{" "}
+          <span>
+            <img
+              width="25"
+              height="25"
+              src="https://github.com/lerodriguezreyes/react-kanban/blob/main/images/love.png?raw=true"
+              alt="filled-like"
+            />
+          </span>{" "}
+          by Luis Emmanuel
+        </p>
+        <p Id="p2">
+          <a
+            href="https://github.com/lerodriguezreyes/final-client"
+            target="_blank"
+          >
+            {" "}
+            SPA Repository{" "}
+          </a>
+          <a
+            href="https://github.com/lerodriguezreyes/final-server"
+            target="_blank"
+          >
+            {" "}
+            Server repository{" "}
+          </a>
+          <a href="mailto:lerodriguezreyes@outlook.com?subject=About%20is%20Democracy%20%20">
+            {" "}
+            Reach Out!{" "}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
