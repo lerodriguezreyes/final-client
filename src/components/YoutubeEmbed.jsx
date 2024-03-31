998ede893eea170618b52f81f8e5551aa450df68
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-31 14:09:30 -0400

    added cutout style to video

--- src/components/YoutubeEmbed.jsx
+++ src/components/YoutubeEmbed.jsx
@@ -1,22 +1,22 @@
-import React from "react";
 import PropTypes from "prop-types";
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
+  <div className="cutoutShadow">
+    <div className="cutout">
+      <div className="video-responsive">
+        <iframe
+          src={`https://www.youtube.com/embed/${embedId}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </div>
+    </div>
   </div>
 );
 
 YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
+  embedId: PropTypes.string.isRequired,
 };
 
 export default YoutubeEmbed;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import YoutubeEmbed from "../components/YoutubeEmbed";
+import "../styles/home.css"
 
 
 function HomePage() {
@@ -6,7 +7,10 @@ function HomePage() {
     <div>
       <h2>So this is how "they" make bills...</h2>
       <YoutubeEmbed embedId="tyeJ55o3El0"/>
-      <h2>| 290,007 bills in 50 years | Where do we fit in this process? |</h2>
+      <div>
+      <h2> 290,007 bills in 50 years </h2> 
+      <h2>Where do we fit in this process? |</h2>
+      </div>
       <h2>Let's talk about this!</h2>
     </div>
   );
