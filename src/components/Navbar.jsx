844f495e29ed6da52c75a20fac5c9b07cf15030f
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-31 14:09:07 -0400

    fworking on nav style. Need to continue responsiveness.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 // src/components/Navbar.jsx
 
 import { Link } from "react-router-dom";
-import { useContext } from "react"; // <== IMPORT
-import { AuthContext } from "../context/auth.context"; // <== IMPORT
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { elastic as Menu } from "react-burger-menu";
+import "../styles/navBar.css";
 
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext); // <== ADD
@@ -11,49 +13,97 @@ function Navbar() {
     return localStorage.getItem("authToken");
   };
   return (
-    <nav>
-      {/* <h1 style={{textAlign:"center" }}> <img style={{height:"150px" }}src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711642893/quoteFrame_gcgjen.png" alt="quote frame"/> 
-    isDemocracy ||
-    <img style={{height:"100px" }} src='https://res.cloudinary.com/dw1igjvgi/image/upload/v1711639080/dadaFrame_nwectm.png' alt="Dada in bold black letters" />
-    </h1> */}
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-      -
-      <Link to="/billlookup">
-        <button>Bill Search</button>
-      </Link>
-      -
-      <Link to="/forum">
-        <button>BuzzBoard</button>
-      </Link>
-      -
-      <Link to="/dadaists">
-        <button>Our community </button>
-      </Link>
-      -
-      {getToken() ? (
-        <>
-          <Link to="/user">
-            <button>UserDashboard</button>
-          </Link>
-          -<button onClick={logOutUser}>Logout</button>
-          <span> Welcome back </span>
-          <span>{user && user.name}</span>
-          <span>!</span>
-        </>
-      ) : (
-        <>
-          <Link to="/signup">
-            <button>Sign Up</button>
-          </Link>
-          -
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-        </>
-      )}
-    </nav>
+    <div id="outer-container">
+      <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+      <main id="page-wrap">
+        <nav className="Navcontainer">
+          <img
+            id="navFrame"
+            src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711642893/quoteFrame_gcgjen.png"
+            alt="quote frame"
+          />
+          <div id="navHeader">
+            <h1>
+              isDemocracy? ||
+              <img
+                id="dadaHand"
+                style={{ height: "100px" }}
+                src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711639080/dadaFrame_nwectm.png"
+                alt="Dada in bold black letters"
+              />
+              <img
+                id="dadaHandBurger"
+                style={{ height: "75px" }}
+                src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1711639080/dadaFrame_nwectm.png"
+                alt="Dada in bold black letters"
+              />
+            </h1>
+          </div>
+          <ul>
+            <li>
+              <Link to="/">
+                <button>Home</button>
+              </Link>
+              ☚
+            </li>
+            <li>
+              <Link to="/billlookup">
+                <button>Bill Search</button>
+              </Link>
+              ☚
+            </li>
+            <li>
+              <Link to="/forum">
+                <button>BuzzBoard</button>
+              </Link>
+              ☚
+            </li>
+            <li>
+              <Link to="/dadaists">
+                <button>Our community </button>
+              </Link>
+              ☚
+            </li>
+            {getToken() ? (
+              <>
+                <li>
+                  <Link to="/user">
+                    <button>UserDashboard</button>
+                  </Link>
+                  ☚
+                </li>
+                <li>
+                  <button onClick={logOutUser}>Logout</button>☚
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/signup">
+                    <button>Sign Up</button>
+                  </Link>
+                  ☚
+                </li>
+                <li>
+                  <Link to="/login">
+                    <button>Login</button>
+                  </Link>
+                  ☚
+                </li>
+              </>
+            )}
+          </ul>
+          {getToken() ? (
+            <>
+              <h2 className="welcome">
+                Welcome back
+                <span> {user && user.name}</span>!{" "}
+              </h2>
+            </>
+          ) : null}
+        </nav>
+      </main>
+    </div>
   );
 }
 
