042bc4e5272a94b8ae9835dd82913b05908d1510
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-01 01:25:38 -0400

    Basic styling, need to fix up the cards.

--- src/pages/BillLookup.jsx
+++ src/pages/BillLookup.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ProPub from '../components/ProPub'
+import "../styles/billSearch.css"
 
 function BillLookup() {
   return (
-    <div>
-    <h2>BillLookup</h2>
+    <div className='billSearch'>
+    <div className='cutoutShadow'>
+    <h2 className='cutout'>BillLookup</h2>
+    </div>
     <ProPub />
     </div>
   )
