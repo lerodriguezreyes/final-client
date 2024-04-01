13186349dda7e6e0d3ed3361c5eca3be82dbb378
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-01 01:25:03 -0400

    styling almost done - need to make it a grid.

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import { get } from "../services/authService";
+import { useEffect, useState } from "react";
+import "../styles/community.css";
 
 function Community() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    get("/users")
+      .then((response) => {
+        console.log("These are the users ===>", response.data);
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting community ===>", error);
+      });
+  }, []);
+
   return (
-    <div>
-    <h1>We are DADA</h1>
-    {/*  user card  here */}
+    <div id="community">
+      <h1>Dadaists of the World, unite!</h1>
+      {users.length > 0 && (
+        <>
+          {users.map((user) => {
+            return (
+              <div className="userCard">
+                <div className="cardContainer" key={user._id}>
+                  <p className="cardText1"> Mi name is {user.name}. 
+                  <br/> Reach out - {user.email}</p>
+                  <img className="image" src={user.profilePicURL} />
+                </div>
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Community
+export default Community;
