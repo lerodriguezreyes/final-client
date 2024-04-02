bc131be0344031faa3b7a504a08fc1f5502d109e
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-02 15:41:00 -0400

    edit comment structure set.

--- src/pages/EditComment.jsx
+++ src/pages/EditComment.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { post } from "../services/authService";
 
 function EditComment() {
-  return (
-    <div>EditComment</div>
-  )
-}
+    const [editComment, setEditComment] = useState("");
+    const navigate = useNavigate();
+  
+    const handleChange = (e) => {
+      setEditComment(e.target.value);
+    };
 
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    // useEffect(() => {
+    //   }, []);
+    
+    return (
+      <div className="addComment">
+        <h2 className="formHeader"> Edit comment </h2>
+        <form onSubmit={handleSubmit}>
+          <label className="row"> Express yourself! </label>
+          <textarea
+            className="row"
+            type="text"
+            name="comment"
+            value={comment.comment}
+            onChange={(e) => handleChange(e)}
+          />
+          <br />
+          <button className="submitButton" type="submit">
+            {" "}
+            Speak your mind!{" "}
+          </button>
+        </form>
+      </div>
+    );
+  }
+  
+  
 export default EditComment
+
+
+
