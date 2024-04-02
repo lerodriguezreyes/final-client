271c0c54d24a714d08a76f4276d98632022da0b6
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-02 15:40:45 -0400

    Forum page structure set.

--- src/pages/Forum.jsx
+++ src/pages/Forum.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { get } from "../services/authService";
+import { returnReadableTimeOnlyDate } from "../services/time";
 
 function Forum() {
+  const [allBills, setAllBills] = useState([]);
+
+  useEffect(() => {
+    get("/bills/")
+      .then((response) => {
+        console.log("These are the bills ===>", response.data);
+        setAllBills(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting all the bills ===>", error);
+      });
+  }, []);
+  console.log("allBills are here", allBills);
   return (
-    <div>Forum</div>
-  )
+    <div id="BuzzBoard">
+      <h1>Dadaists of the World, speak your mind!</h1>
+      {allBills.length > 0 && (
+        <>
+          {allBills.map((bill) => {
+            return (
+              <div key={bill.title}>
+                <div>
+                  <p>
+                    {" "}
+                    {bill.title} </p>
+                    <p> Conversation start:{" "}
+                    {returnReadableTimeOnlyDate(bill.createdAt)} </p>
+                    <p>Last comment:{" "}
+                    {returnReadableTimeOnlyDate(bill.updatedAt)}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Forum
+export default Forum;
