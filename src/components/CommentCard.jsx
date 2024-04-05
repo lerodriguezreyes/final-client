f4698b88f9709d4c4116de5c49756ccf7010f1b7
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-04-05 11:02:39 -0400

    preliminary comment logic missing the update moment.

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,13 +1,26 @@
 import { returnReadableTime } from "../services/time";
 import "../styles/commentCard.css";
-import { axiosDelete } from "../services/authService";
-import { useContext, useState } from "react";
+import { axiosDelete, post } from "../services/authService";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../context/auth.context";
 
 function CommentCard({ getBillDetails, setGetBillDetails }) {
-  const [upVote, setUpVote] = useState(0);
-  const [downVote, setDownVote] = useState(0);
   const { user } = useContext(AuthContext);
+  const [editing, setEditing] = useState({ editing: false, index: null });
+  const [commentToEdit, setCommentToEdit] = useState(null);
+
+  // const handleSubmit = (e, index) => {
+  //   e.preventDefault();
+  //   console.log(getBillDetails.comments[index]._id);
+  //   // post(`/comments/update/${getBillDetails.comments[index]._id}`, commentToEdit)
+  //   //   .then((response) => {
+  //   //     console.log("Edited comment", response.data);
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     const errorDescription = error.response.data.message;
+  //   //     setErrorMessage(errorDescription);
+  //   //   });
+  // };
 
   const handleDelete = (comment) => {
     axiosDelete(`/comments/delete/${comment._id}`)
@@ -15,46 +28,66 @@ function CommentCard({ getBillDetails, setGetBillDetails }) {
       .catch((error) => console.log(error));
   };
 
-  const handleEdit = (comment) => {
-    console.log("This is the comment ID ===>", comment._id);
+  const handleEdit = (comment, index) => {
+    setEditing({
+      editing: editing.editing ? false : true,
+      index: index || index === 0 ? index : null,
+    });
+    setCommentToEdit(comment.comment);
   };
 
+  useEffect(() => {
+    console.log(editing);
+  }, [editing]);
   return (
-    <div>
+    <>
       {getBillDetails.comments.map((comment, index) => {
         return (
           <div className="commentCard" key={comment._id}>
             <div className="cardContainer">
-              <div>
-                <img
-                  className="commentImage"
-                  src={comment.owner.profilePicURL}
-                />
-                {user._id === comment.owner._id && (
-                  <>
+              <img className="userprofile" src={comment.owner.profilePicURL} />
+              {user._id === comment.owner._id && (
+                <>
+                  <img
+                    className="deleteicon"
+                    onClick={() => handleDelete(comment)}
+                    src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1712249139/vsae7bau37zdyvuibmfw.png"
+                  />
+                  {editing.editing && editing.index === index ? (
+                    <span onClick={() => handleEdit(comment, index)}>❌</span>
+                  ) : (
                     <img
-                      onClick={() => handleDelete(comment)}
-                      src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1712249139/vsae7bau37zdyvuibmfw.png"
-                    />
-                    <img
-                      onClick={() => handleEdit(comment)}
+                      className="editicon"
+                      onClick={() => handleEdit(comment, index)}
                       src="https://res.cloudinary.com/dw1igjvgi/image/upload/v1712249175/cl827gcafqrkngvbcdmh.png"
                     />
-                  </>
-                )}
-                <p className="commentText"> {comment.owner.name} says: </p>
-                <div></div>
-              </div>
+                  )}
+                </>
+              )}
+              <p className="commentowner"> {comment.owner.name} says: </p>
               <div>
-                <h4>{comment.comment} </h4>
-                <h5>{returnReadableTime(comment.createdAt)} </h5>{" "}
+                {editing.editing && editing.index === index ? (
+                  <form>
+                    <input
+                      type="text"
+                      name="comment"
+                      value={commentToEdit}
+                      onChange={(e) => setCommentToEdit(e.target.value)}
+                    />
+                    <button type="submit">edit comment</button>
+                  </form>
+                ) : (
+                  <h4 className="commenttext">{comment.comment} </h4>
+                )}
+                <h5 className="commentdate">
+                  {returnReadableTime(comment.createdAt)}{" "}
+                </h5>{" "}
               </div>
-              {/* { {comment.updatedAt} ? <p>{comment.updatedAt}</p> : <p>{comment.createdAt}</p>} */}
             </div>
           </div>
         );
       })}
-    </div>
+    </>
   );
 }
 
